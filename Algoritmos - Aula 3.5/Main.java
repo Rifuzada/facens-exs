cfb4c9e9352bcf00955f89967f8e7869724b0002
Aluno <aluno@facens.br>
2024-09-05 22:27:45 -0300

    commit final

--- Algoritmos - Aula 3.5/Main.java
+++ Algoritmos - Aula 3.5/Main.java
@@ -1,8 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-    Scanner scn =  new Scanner(System.in);
-    public static void main(Strings[] args){
-       
+    public static void main(String[] args){
+        Scanner scn =  new Scanner(System.in);
+        
+        int primeiroValor = 0, segundoValor = 0, resultado = 0; //int pq é soma não outro tipo de operação
+        
+        /*
+        OU
+            int primeiroValor = 0;
+            int segundoValor = 0;
+            int resultado = 0;
+        */
+        System.out.println("Digite o primeiro numero da soma: ");
+        primeiroValor = scn.nextInt();
+        System.out.println("Digite o segundo numero da soma: ");
+        segundoValor = scn.nextInt();
+        resultado = primeiroValor + segundoValor;
+        System.out.println("O resultado da soma é " + resultado);
     }
 }
