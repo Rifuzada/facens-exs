1da7363c9814fefe2a4b45086ef61edeae685879
Aluno <aluno@facens.br>
2024-09-12 21:12:49 -0300

    alterar nomes e add aula 4B

---
+++ Algoritmos - Aula 4A/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 2;
+        if(n < 10 && n > 0){
+            System.out.println(n + "Está entre 0 e 10");
+        }
+        else{
+            System.out.println(n + "Não está entre 0 e 10");
+        }
+    }
+}
