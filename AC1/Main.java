f44e0b50acc699a28f623536c2eeb0c19af12717
Rifu <rifuperez@gmail.com>
2024-09-20 08:22:22 -0300

    correcao formula

--- AC1/Main.java
+++ AC1/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         double AC1, AC2, AG, AF, formula, media;
@@ -16,17 +16,16 @@ public class Main{
         AG = scn.nextDouble();
         System.out.println("Digite a nota da AF: ");
         AF = scn.nextDouble();
-        formula = AC1 * 0.15 + AC2 * 0.3 + AG * 0.10 + AF * 0.45;
+        formula = (AC1 * 0.15) + (AC2 * 0.3) + (AG * 0.10) + (AF * 0.45);
         System.out.println("-----CALCULADORA DE MEDIA ALUNOS-----");
-        
-        if(formula >= media){
+
+        if (formula >= media) {
             System.out.println("\nSua nota final foi: " + formula + " e a media é " + media);
             System.out.println("\nParabens voce foi aprovado!!!");
-        }else if(formula < media){
+        } else if (formula < media) {
             System.out.println("\nSua nota final foi: " + formula + " e a media é " + media);
             System.out.println("\nQue pena! voce foi reprovado!!!");
         }
     }
 }
-//AC1 * 15% + AC2 * 30% + AG * 10% + AF * 45%
-
+// AC1 * 15% + AC2 * 30% + AG * 10% + AF * 45%
