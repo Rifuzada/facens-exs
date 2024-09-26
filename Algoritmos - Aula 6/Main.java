b12f0e4146fa9e6cd3b27486e29810ef418eabcf
Aluno <aluno@facens.br>
2024-09-26 20:31:08 -0300

    aula6 criada

--- Algoritmos - Aula 6/Main.java
+++ Algoritmos - Aula 6/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-      
+        int n = 1;
+    if(n == 0 ){
+        System.out.println("Numero Nulo.");
+    }else if(n > 0 ){
+        System.out.println("Numero positio.");
+    }else{
+        System.out.println("Numero negatio.");
+    }
     }
 }
