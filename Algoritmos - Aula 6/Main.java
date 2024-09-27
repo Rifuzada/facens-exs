62c878ceba1432b2db117087b75fca17b40b71b2
Aluno <aluno@facens.br>
2024-09-26 21:37:47 -0300

    Fim da aula 6

--- Algoritmos - Aula 6/Main.java
+++ Algoritmos - Aula 6/Main.java
@@ -1,12 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 1;
+    int n = 1;
     if(n == 0 ){
         System.out.println("Numero Nulo.");
     }else if(n > 0 ){
-        System.out.println("Numero positio.");
+        System.out.println("Numero positivo.");
     }else{
-        System.out.println("Numero negatio.");
+        System.out.println("Numero negativo.");
     }
+    //OU, caso não precise de condições booleanas e somente necessite verificar se um numero é igual a outro numero. Não sendo variavel, compare uma variavel com um numero ja existente, como:
+    /* n = 1
+    *
+    */
+    switch(n){
+        case 1:
+            System.out.println("Coca") ;
+            break;
+        case 2:
+            System.out.println("Fanta");
+            break;
+        case 3:
+            System.out.println("Kuat");
+            break;
+        default:
+            System.out.println("Opção invalida");
+            break;
     }
+    } 
 }
