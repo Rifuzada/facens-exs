a10495da9394517ba1e01bc41edf71f75e023cf0
Aluno <aluno@facens.br>
2024-10-10 19:12:28 -0300

    fixed fatorial

--- exs/Fatorial/Main.java
+++ exs/Fatorial/Main.java
@@ -9,10 +9,14 @@ public class Main {
         numero = scn.nextInt();
         int cont = numero;
         while (cont > 1){ // Enquanto x for menor que 1 faça o que está entre as chaves
-            System.out.println(cont);
+            //System.out.println(cont);
             numero = numero *(cont-1); // A variável temporária ira receber o resultado da multiplicação dela, pelo valor de x menos 1
             cont--; // aqui decrementamos o valor de x em um, no final do loop
+            if(cont == 1){
+                break;
+            }
             System.out.println(numero); // Esse comando imprime o valor de numero. O último será o valor final do Fatorial.
+            
         }
     }
 }
