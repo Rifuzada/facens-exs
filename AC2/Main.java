2aeec459b5b268946aab1f86363f9a23a85e36a6
Rifu <rifuperez@gmail.com>
2024-11-06 15:45:31 -0300

    AC2 modificada

--- AC2/Main.java
+++ AC2/Main.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
-//Refazer codigo ac1 + simulado ac2
+/*
+ * Nome: Rafael Perez Viana
+ * RA: 240169
+ * Turma: Semipresencial - ADS
+ * 
+ * Nome: Lucas Adriano De Oliveira Balbino
+ * RA: 240286
+ * Turma: Semipresencial - ADS
+ */
 
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        double ac1, ac2, ag, af, formula, media, pesoAc1, pesoAc2, pesoAg, pesoAf;
+        double ac1, ac2, ag, af, formula, media, pesoAc1, pesoAc2, pesoAg, pesoAf, pesoSomado;
         String continuar;
 
         System.out.println("Digite a media minima: ");
@@ -18,32 +26,37 @@ public class Main {
         pesoAg = scn.nextDouble();
         System.out.println("Defina o peso da AF: ");
         pesoAf = scn.nextDouble();
-        // deixo fora do loop dowhile para nao ficar repetindo toda vez que precisar
-        // calcular nota.
-        do {
-            System.out.println("-----CALCULADORA DE MEDIA ALUNOS-----");
-            System.out.println("Digite a nota da AC1: ");
-            ac1 = scn.nextDouble();
-            System.out.println("Digite a nota da AC2: ");
-            ac2 = scn.nextDouble();
-            System.out.println("Digite a nota da AG: ");
-            ag = scn.nextDouble();
-            System.out.println("Digite a nota da AF: ");
-            af = scn.nextDouble();
-            formula = (ac1 * pesoAc1) + (ac2 * pesoAc2) + (ag * pesoAg) + (af * pesoAf);
-            System.out.println("-----CALCULADORA DE MEDIA ALUNOS-----");
-            if (formula >= media) {
-                System.out.println("\nSua nota final foi: " + formula + " e a media é " + media);
-                System.out.println("\nParabens voce foi aprovado!!!");
-            } else if (formula < media) {
-                System.out.println("\nSua nota final foi: " + formula + " e a media é " + media);
-                System.out.println("\nQue pena! voce foi reprovado!!!");
-            } else if (formula < 10) {
-                System.out.println("Sem mentir, essa nota não é possível.");
-            }
-            System.out.println("Deseja realizar um novo controle de notas? (S/N): ");
-            continuar = scn.next().toUpperCase();
-        } while (continuar.equals("S"));
+
+        pesoSomado = pesoAc1 + pesoAc2 + pesoAg + pesoAf;
+
+        if (pesoSomado == 1) {
+            do {
+                System.out.println("-----CALCULADORA DE MEDIA ALUNOS-----");
+                System.out.println("Digite a nota da AC1: ");
+                ac1 = scn.nextDouble();
+                System.out.println("Digite a nota da AC2: ");
+                ac2 = scn.nextDouble();
+                System.out.println("Digite a nota da AG: ");
+                ag = scn.nextDouble();
+                System.out.println("Digite a nota da AF: ");
+                af = scn.nextDouble();
+                formula = (ac1 * pesoAc1) + (ac2 * pesoAc2) + (ag * pesoAg) + (af * pesoAf);
+                System.out.println("-----CALCULADORA DE MEDIA ALUNOS-----");
+                if (formula >= media) {
+                    System.out.println("Sua nota final foi: " + formula + " e a media é " + media);
+                    System.out.println("Parabens voce foi aprovado!!!");
+                } else if (formula < media) {
+                    System.out.println("Sua nota final foi: " + formula + " e a media é " + media);
+                    System.out.println("Que pena! voce foi reprovado!!!");
+                } else if (formula < 10) {
+                    System.out.println("Sem mentir, essa nota não é possível.");
+                }
+                System.out.println("Deseja realizar um novo controle de notas? (S/N): ");
+                continuar = scn.next().toUpperCase();
+            } while (continuar.equals("S"));
+        } else {
+            System.out.println("A soma dos pesos tem que resultar em 1.");
+        }
         scn.close();
     }
 }
