dd0dadd833592610627b77fc7bd0bac44859da6c
Aluno <aluno@facens.br>
2024-11-14 21:52:24 -0300

    aula 12B - Matrizes

--- Algoritmos - Aula 12A/Main.java
+++ Algoritmos - Aula 12A/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);    
+        // char [] letras = new char[3];
+        // System.out.println("Digite 3 letras separadamente");
+        // for(int i = 0; i <= 2 ;i++){
             
+        //     letras[i] = scn.next().charAt(0);
+        //     System.out.println(letras[i]);
+        // }
+        int[] vetorInt = {1,2,3,4};
+        for(int i = 0; i < 4; i++){
+            vetorInt[i] *= 2;
+            System.out.println(vetorInt[i]);
+        }
+        scn.close();
     }
 }
---
+++ Algoritmos - Aula 12B/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        int[][] matrizInt = {{1,2},{1,3}};
+        for(int i = 0; i < 2;i++){
+            for(int y = 0; y < 2; y++){
+                System.out.println(matrizInt[i][y]);
+            }
+        }
+    }
+}
