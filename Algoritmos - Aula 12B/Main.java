b4c8619fecb46d615e61a606a2c244a7dc720602
Aluno <aluno@facens.br>
2024-11-28 19:11:21 -0300

    concertei algo da aula 12A

--- Algoritmos - Aula 12B/Main.java
+++ Algoritmos - Aula 12B/Main.java
@@ -1,10 +1,11 @@
 public class Main{
     public static void main(String[] args) {
         int[][] matrizInt = {{1,2},{1,3}};
-        for(int i = 0; i < 2;i++){
-            for(int y = 0; y < 2; y++){
-                System.out.println(matrizInt[i][y]);
+        for(int i = 0; i < matrizInt.length;i++){
+            for(int y = 0; y < matrizInt[i].length; y++){
+                System.out.print(matrizInt[i][y]+" ");
             }
+            System.out.println();   
         }
     }
 }
